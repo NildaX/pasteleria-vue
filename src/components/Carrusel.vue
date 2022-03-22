8e5bee8b67f75ac86ed0d3f9b8c0b0284541209c
NildaX <nilda_gaby_9745@live.com.mx>
2022-03-21 20:21:53 -0600

    boot

---
+++ src/components/Carrusel.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="@/assets/cake5.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="@/assets/cake3.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="@/assets/cake3.jpg" alt="Third slide">
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default{
+    name: 'Carrusel',
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,36 @@
 <template>
-  <div>
-    <h2>Testing Bootstrap buttons</h2>
-    <button class="btn btn-primary">Test</button>
-    <button class="btn btn-info">Test</button>
-    <button class="btn btn-danger">Test</button>
-    <button class="btn btn-secondary">Test</button>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-      <strong>Hi!</strong> I'm Sunil
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">×</span>
-      </button>
+  <div class="home">
+    <Header/>
+    <div class="fondorosa">
+      <Carrusel/>
+      <Botones/>
+      <Informacion/>
     </div>
   </div>
 </template>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "../components/Header.vue";
+import Carrusel from "../components/Carrusel.vue";
+import Botones from "../components/Botones.vue";
+import Informacion from "../components/Informacion.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Header,
+    Carrusel,
+    Botones,
+    Informacion,
   }
 }
 </script>
+<style>
+.fondorosa{
+  background-color:#FFD1DC;
+}
+.fondoblanco{
+  background-color:#FFFFFF;
+}
+</style>
