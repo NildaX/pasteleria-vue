381eca733125e2d22a5e85142f1d8714ba5f22e7
NildaX <nilda_gaby_9745@live.com.mx>
2022-03-21 19:38:42 -0600

    con boot

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Testing Bootstrap buttons</h2>
+    <button class="btn btn-primary">Test</button>
+    <button class="btn btn-info">Test</button>
+    <button class="btn btn-danger">Test</button>
+    <button class="btn btn-secondary">Test</button>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>Hi!</strong> I'm Sunil
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+      </button>
+    </div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
