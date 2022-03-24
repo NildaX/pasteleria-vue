9ae5eacc3e8f5544b59650e91ae7c1760fc62f49
NildaX <nilda_gaby_9745@live.com.mx>
2022-03-23 20:04:08 -0600

    final

--- src/store/index.js
+++ src/store/index.js
@@ -4,18 +4,18 @@ export default createStore({
   state: {
     estadoenvio: " ",
     existenciasP:[
-      {id:0,nombre:"Chocolate",cantidad:"0"},
-      {id:1,nombre:"Fresa",cantidad:"0"},
-      {id:2,nombre:"Tres leches",cantidad:"0"},
-      {id:3,nombre:"Platano",cantidad:"0"},
-      {id:4,nombre:"Chessecake",cantidad:"0"},
+      {id:0,nombre:"Chocolate",cantidad:"10"},
+      {id:1,nombre:"Fresa",cantidad:"7"},
+      {id:2,nombre:"Tres leches",cantidad:"8"},
+      {id:3,nombre:"Platano",cantidad:"9"},
+      {id:4,nombre:"Chessecake",cantidad:"10"},
     ],
     existenciasA:[
-      {id:5,nombre:"Galletas",cantidad:"0"},
-      {id:6,nombre:"Gomitas",cantidad:"0"},
-      {id:7,nombre:"Gelatina",cantidad:"0"},
-      {id:8,nombre:"Frutos rojos",cantidad:"0"},
-      {id:9,nombre:"M&M",cantidad:"0"},
+      {id:5,nombre:"Galletas",cantidad:"15"},
+      {id:6,nombre:"Gomitas",cantidad:"15"},
+      {id:7,nombre:"Gelatina",cantidad:"15"},
+      {id:8,nombre:"Frutos rojos",cantidad:"10"},
+      {id:9,nombre:"M&M",cantidad:"5"},
     ],
     pedidos:[
     {id:0,descripcion:"Sabor Chocolate con Fresa con adornos de galletas y gomitas",nombre:"Raúl R",telefono:"111 222 33 44",correo:"raulr@prueba.com"},
